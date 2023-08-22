92a82d043839d7d68624f5879288a4868970c310
eduardop1p <el702985@gmail.com>
2023-08-22 09:20:18 -0300

    add files

---
+++ src/components/mansory/index.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+
+import Image from 'next/image';
+import { useEffect, useState, useCallback } from 'react';
+
+import { MasonryContainer } from './styled';
+
+interface Props {
+  photos: any;
+}
+
+export default function MasonryPin({ photos }: Props) {
+  const [columnCount, setColumnCount] = useState(6);
+  const [columnWidth, setColumnWidth] = useState(6.5);
+
+  const newPhotos = [];
+  const photosArrayLength = Math.ceil(photos.length / columnCount);
+  for (let i = 0; i < photos.length; i += photosArrayLength) {
+    newPhotos.push(photos.slice(i, i + photosArrayLength));
+  }
+
+  const handleLoadImg = useCallback(
+    (img: HTMLImageElement) => {
+      const parent = img.parentElement as HTMLDivElement;
+      const windowWidth = document.body.clientWidth;
+      const aspectoRatio = img.naturalWidth / img.naturalHeight;
+      const parentWidth = windowWidth / columnWidth;
+      const parentHeight = parentWidth / aspectoRatio;
+
+      parent.style.width = `${parentWidth.toFixed(2)}px`;
+      parent.style.height = `${parentHeight.toFixed(2)}px`;
+    },
+    [columnWidth]
+  );
+
+  useEffect(() => {
+    window.onresize = () => {
+      document.querySelectorAll('.pin').forEach((img: Element) => {
+        handleLoadImg(img as HTMLImageElement);
+      });
+    };
+  }, [handleLoadImg]);
+
+  return (
+    <div>
+      <h2
+        style={{
+          textAlign: 'center',
+          margin: '2rem 0',
+          fontSize: '1rem',
+        }}
+      >
+        Masonry gallery
+      </h2>
+      <MasonryContainer $columnWidth={columnWidth} $marginColumn="1rem">
+        {newPhotos.map((arrays: any, index: number) => (
+          <div key={index} className="masonry-column">
+            {arrays.map((value: any) => (
+              <div key={value.id} className="pin-container">
+                <Image
+                  className="pin"
+                  src={value.src.medium}
+                  alt={value.alt}
+                  fill
+                  sizes="100%"
+                  onLoadingComplete={img => handleLoadImg(img)}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+      </MasonryContainer>
+    </div>
+  );
+}
---
+++ src/components/mansory/styled.ts
@@ -0,0 +1,33 @@
+import { styled, css } from 'styled-components';
+
+interface Props {
+  $columnWidth: number;
+  $marginColumn: string;
+}
+
+export const MasonryContainer = styled.div<Props>`
+  ${({ $marginColumn, $columnWidth }) => css`
+    display: flex;
+    justify-content: center;
+    /* padding: 0 1rem; */
+    /* width: 100%; */
+
+    & > :not(:last-child) {
+      margin-right: ${$marginColumn};
+    }
+
+    & > .masonry-column {
+      width: calc(100% / ${$columnWidth});
+      flex: none;
+
+      & > .pin-container {
+        position: relative;
+        margin-bottom: ${$marginColumn};
+
+        & > .pin {
+          border-radius: 1rem;
+        }
+      }
+    }
+  `}
+`;
